b13e4aa1f23e6a20b628584eb24b4090b8db533f
MaxHalford <maxhalford25@gmail.com>
2016-10-24 20:11:23 +0200

    Fix requirements for travis

--- performance.py
+++ performance.py
@@ -3,11 +3,12 @@ import pandas as pd
 from scipy.sparse import rand
 
 import prince
-
+from sklearn.decomposition import PCA
 
 n = int(1e6)
 p = int(1e2)
 density = 0.0001
-data = pd.DataFrame(rand(n, p, density=density).toarray())
+X = pd.DataFrame(rand(n, p, density=density).toarray())
 
-cProfile.run('prince.PCA(data, nbr_components=2)')
+cProfile.run('prince.PCA(X, nbr_components=2)')
+cProfile.run("pca = PCA(svd_solver='randomized', n_components=2); pca.fit(X)")
