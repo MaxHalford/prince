5d7410fd70605cab3f4ffb30162cc86fb5e77659
MaxHalford <maxhalford25@gmail.com>
2016-11-11 19:28:00 +0100

    Delete setup folder

--- setup.py
+++ setup.py
@@ -10,7 +10,13 @@ setup(
     author_email=['axel.bellec@outlook.fr', 'maxhalford25@gmail.com'],
     dependency_links=[],
     description='Factorial analysis in Python',
-    install_requires=open('setup/requirements.txt').read().splitlines(),
+    install_requires=[
+        'fbpca>=1.0',
+        'matplotlib>=1.5.0',
+        'numpy>=1.11.2',
+        'pandas>=0.19.0',
+        'scipy>=0.16.0'
+    ],
     license='MIT',
     name=__project__,
     packages=find_packages(exclude=['tests']),
