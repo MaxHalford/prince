5e6a1680a34afb1af38426282da76bbf30a7a35b
Max Halford <maxhalford25@gmail.com>
2017-03-20 08:56:25 +0100

    Fix setup.py

--- prince/__init__.py
+++ prince/__init__.py
@@ -3,9 +3,3 @@ from prince.famd import FAMD
 from prince.mca import MCA
 from prince.pca import PCA
 from prince.svd import SVD
-
-__title__ = 'prince'
-__version__ = '0.2.5'
-__author__ = 'Max Halford'
-__license__ = 'MIT'
-__copyright__ = 'Copyright 2016 Max Halford'
--- setup.py
+++ setup.py
@@ -2,11 +2,9 @@
 
 from setuptools import setup, find_packages
 
-from prince import  __author__, __license__, __title__, __version__
-
 
 setup(
-    author=__author__,
+    author='Max Halford',
     author_email='maxhalford25@gmail.com',
     description='Factor analysis for in Python',
     install_requires=[
@@ -16,9 +14,9 @@ setup(
         'pandas>=0.18.0',
         'scipy>=0.16.0'
     ],
-    license=__license__,
-    name=__title__,
+    license='MIT',
+    name='prince',
     packages=find_packages(exclude=['tests']),
     url='https://github.com/MaxHalford/Prince',
-    version=__version__,
+    version='0.2.6',
 )
